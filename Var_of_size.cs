df6c0a9f9ed610f0a44ff0fbcfb9829444866c54
F_D <den9152@yandex.ru>
2022-11-18 20:36:19 +0700

    Initial commit

---
+++ Var_of_size.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_18_11_2022_Peremennaya
+{
+    internal class Var_of_size <T>
+    {
+        // параметр типа используется для задания типа свойства
+        public T X { get; set; }
+    }
+
+}
