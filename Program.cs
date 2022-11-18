2bc75807aade4c32941dc5cb8d29a02bf64c985a
F_D <den9152@yandex.ru>
2022-11-18 21:00:42 +0700

    2
    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Создать generic делегат, которая принимает одну переменную.
+//Создать класс "переменная на размер", которая имеет методы для нескольких типов данных
+//(int, double, decimal, char, string). Метод выводит переменную
+//на столько раз, сколько данная переменная занимает в памяти.
+//Для дробных чисел создать вывод трех знаков после запятой.
+using sharp_18_11_2022_Peremennaya;
+
+Var_of_size <int>f = new();
+Var_of_size <char> h= new();
+Var_of_size<double> v= new();
+Var_of_size<string> s= new();
+
+
+public delegate double first = f;
+
--- Var_of_size.cs
+++ Var_of_size.cs
@@ -10,9 +10,20 @@ namespace sharp_18_11_2022_Peremennaya
     {
         // параметр типа используется для задания типа свойства
         public T X { get; set; }
+        public Var_of_size() { }
+       
+        public Var_of_size(T x) { X = x; }
 
         public void met()
         {
+            Console.WriteLine(X);
+        }
+        public void met2(string k)
+        {
+            Console.WriteLine(
+                System.Text.ASCIIEncoding.Unicode.GetByteCount(k)
+                );
+
 
         }
     }
